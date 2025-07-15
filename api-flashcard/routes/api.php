d3c34c72939e10296cd4170c4f2339fdd0ed5f32
iasmim <iasmimoliveiraestudos@gmail.com>
2025-07-14 22:21:17 -0300

    build: changing docker image and permissions

--- api-flashcard/routes/api.php
+++ api-flashcard/routes/api.php
@@ -5,5 +5,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/restaurants/search-by-location', [RestaurantSearchController::class, 'searchByLocation']);
 });
